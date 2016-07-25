a6b620a9d30c8c5d6e9eb44939bb40961f743b40
Jeremy Kong <jeremykong@hotmail.com>
2016-07-25 12:08:54 +0100

    Added source check ability to pi messages

--- idapi/src/com/ictools/idapi/bayesnet/PiMessage.java
+++ idapi/src/com/ictools/idapi/bayesnet/PiMessage.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 public class PiMessage {
     private final List<Double> piMessage;
+    private final Node source;
 
-    public PiMessage(List<Double> piMessage) {
+    public PiMessage(List<Double> piMessage, Node source) {
         this.piMessage = piMessage;
+        this.source = source;
     }
 
     public List<Double> getPiMessage() {
         return piMessage;
+    }
+
+    public Node getSource() {
+        return source;
     }
 }
